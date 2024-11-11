ef6d5c34c2c9b3f77c759bceaf47b0f334c06832
Nahu <nahuel11905@gmail.com>
2024-11-11 13:25:38 -0300

    Revert "Subiendo el archivo de la BD"
    se subieron archivos de mas entonces no los quiero subir

--- .Net/InmobiliariaAPI-Vargas/obj/Debug/net8.0/InmobiliariaAPI-Vargas.AssemblyInfo.cs
+++ .Net/InmobiliariaAPI-Vargas/obj/Debug/net8.0/InmobiliariaAPI-Vargas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InmobiliariaAPI-Vargas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be606312a6033f91b42a476b8057b131dd1aa9d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3211e0a9d567cdbed3ef33002cac2d853af83d")]
 [assembly: System.Reflection.AssemblyProductAttribute("InmobiliariaAPI-Vargas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InmobiliariaAPI-Vargas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
